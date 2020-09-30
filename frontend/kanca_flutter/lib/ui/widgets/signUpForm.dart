32ac08c555830dc27e1f6dffba9d564b2c76bbda
jevoned <48shield@gmail.com>
2020-09-30 19:29:51 +0700

    Update signUpForm.dart

--- frontend/kanca_flutter/lib/ui/widgets/signUpForm.dart
+++ frontend/kanca_flutter/lib/ui/widgets/signUpForm.dart
@@ -48,3 +48,178 @@ class _SignUpFormState extends State<SignUpForm> {
           email: _emailController.text, password: _passwordController.text),
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return BlocListener<SignUpBloc, SignUpState>(
+      listener: (BuildContext context, SignUpState state) {
+        if (state.isFailure) {
+          Scaffold.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(
+                content: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Sign Up Failed"),
+                    Icon(Icons.error),
+                  ],
+                ),
+              ),
+            );
+        }
+        if (state.isSubmitting) {
+          print("isSubmitting");
+          Scaffold.of(context)
+            ..showSnackBar(
+              SnackBar(
+                content: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Signing up..."),
+                    CircularProgressIndicator(),
+                  ],
+                ),
+              ),
+            );
+        }
+        if (state.isSuccess) {
+          print("Success");
+          BlocProvider.of<AuthenticationBloc>(context).add(LoggedIn());
+          Navigator.of(context).pop();
+        }
+      },
+      child: BlocBuilder<SignUpBloc, SignUpState>(
+        builder: (context, state) {
+          return SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Container(
+              color: backgroundColor,
+              width: size.width,
+              height: size.height,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Center(
+                    child: Text(
+                      "Chill",
+                      style: TextStyle(
+                          fontSize: size.width * 0.2, color: Colors.white),
+                    ),
+                  ),
+                  Container(
+                    width: size.width * 0.8,
+                    child: Divider(
+                      height: size.height * 0.05,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(size.height * 0.02),
+                    child: TextFormField(
+                      controller: _emailController,
+                      autovalidate: true,
+                      validator: (_) {
+                        return !state.isEmailValid ? "Invalid Email" : null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                            color: Colors.white, fontSize: size.height * 0.03),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(size.height * 0.02),
+                    child: TextFormField(
+                      controller: _passwordController,
+                      autocorrect: false,
+                      obscureText: true,
+                      autovalidate: true,
+                      validator: (_) {
+                        return !state.isPasswordValid
+                            ? "Invalid Password"
+                            : null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                            color: Colors.white, fontSize: size.height * 0.03),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(size.height * 0.02),
+                    child: GestureDetector(
+                      onTap: isSignUpButtonEnabled(state)
+                          ? _onFormSubmitted
+                          : null,
+                      child: Container(
+                        width: size.width * 0.8,
+                        height: size.height * 0.06,
+                        decoration: BoxDecoration(
+                          color: isSignUpButtonEnabled(state)
+                              ? Colors.white
+                              : Colors.grey,
+                          borderRadius:
+                              BorderRadius.circular(size.height * 0.05),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(
+                                fontSize: size.height * 0.025,
+                                color: Colors.blue),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  void _onEmailChanged() {
+    _signUpBloc.add(
+      EmailChanged(email: _emailController.text),
+    );
+  }
+
+  void _onPasswordChanged() {
+    _signUpBloc.add(
+      PasswordChanged(password: _passwordController.text),
+    );
+  }
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    _emailController.dispose();
+
+    super.dispose();
+  }
+}
