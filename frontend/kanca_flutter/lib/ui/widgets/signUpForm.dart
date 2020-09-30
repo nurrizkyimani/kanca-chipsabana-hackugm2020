9367cb1f58ec608e145d55aca735de9288d51515
jevoned <48shield@gmail.com>
2020-09-30 19:14:10 +0700

    Create signUpForm.dart

---
+++ frontend/kanca_flutter/lib/ui/widgets/signUpForm.dart
@@ -0,0 +1,33 @@
+import 'package:chill/bloc/authentication/authentication_bloc.dart';
+import 'package:chill/bloc/authentication/authentication_event.dart';
+import 'package:chill/bloc/signup/bloc.dart';
+import 'package:chill/repositories/userRepository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../constants.dart';
+
+class SignUpForm extends StatefulWidget {
+  final UserRepository _userRepository;
+
+  SignUpForm({@required UserRepository userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository;
+
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  SignUpBloc _signUpBloc;
+  //UserRepository get _userRepository => widget._userRepository;
+
+  bool get isPopulated =>
+      _emailController.text.isNotEmpty && _passwordController.text.isNotEmpty;
+
+  bool isSignUpButtonEnabled(SignUpState state) {
+    return isPopulated && !state.isSubmitting;
+  }
