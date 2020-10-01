61b9448d04152f8c82d9efc70540871ac1016560
jevoned <48shield@gmail.com>
2020-10-02 00:47:53 +0700

    Update HomePage.dart

--- frontend/kanca_flutter/lib/HomePage.dart
+++ frontend/kanca_flutter/lib/HomePage.dart
@@ -11,121 +11,49 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  TabController _tabcontroller;
-  @override
-  void initState() {
-    super.initState();
-    _tabcontroller = new TabController(length: 3, vsync: this);
-    _tabcontroller.addListener(() {
-      setState(() {});
+class _HomePageState extends State<HomePage> {
+  int _selectedTabIndex = 0;
+
+  void _onNavBarTapped(int index) {
+    setState(() {
+      _selectedTabIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-          backgroundColor: Colors.white,
-          automaticallyImplyLeading: false,
-          elevation: 1,
-          title: new TabBar(
-              indicatorColor: Colors.transparent,
-              controller: _tabcontroller,
-              tabs: [
-                new SafeArea(
-                  child: new Container(
-                    padding: EdgeInsets.all(ScreenUtil().setWidth(20.0)),
-                    child: Center(
-                      child: new Icon(
-                        Tinder_clone.iconfinder_icons_user2_1564535,
-                        color: _tabcontroller.index == 0
-                            ? Theme.of(context).primaryColor
-                            : Colors.grey,
-                        size: ScreenUtil().setSp(80.0),
-                      ),
-                    ),
-                  ),
-                ),
-                new SafeArea(
-                  child: new Container(
-                    padding: EdgeInsets.all(ScreenUtil().setWidth(20.0)),
-                    child: Center(
-                      child: new Icon(
-                        Tinder_clone.iconfinder_338_tinder_logo_4375488__1_,
-                        color: _tabcontroller.index == 1
-                            ? Theme.of(context).primaryColor
-                            : Colors.grey,
-                        size: ScreenUtil().setSp(80.0),
-                      ),
-                    ),
-                  ),
-                ),
-                new SafeArea(
-                  child: new Container(
-                    padding: EdgeInsets.all(ScreenUtil().setWidth(20.0)),
-                    child: Center(
-                      child: new Icon(
-                        Tinder_clone.iconfinder_message_01_186393,
-                        color: _tabcontroller.index == 2
-                            ? Theme.of(context).primaryColor
-                            : Colors.grey,
-                        size: ScreenUtil().setSp(80.0),
-                      ),
-                    ),
-                  ),
-                ),
-              ]),
-        ),
-        body: new TabBarView(
-          physics: NeverScrollableScrollPhysics(),
-          controller: _tabcontroller,
-          children: <Widget>[
-            new ProfileTab(),
-            new TinderTab(),
-            new MessagesTab()
-          ],
-        ),
-        int _selectedTabIndex = 0;
+    final _listPage = <Widget>[
+      new ProfileTab(),
+      new TinderTab(),
+      new MessagesTab()
+    ];
 
-        void _onNavBarTapped(int index) {
-          setState(() {
-            _selectedTabIndex = index;
-          });
-        }
+    final _bottomNavBarItems = <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        title: Text('Home'),
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.favorite),
+        title: Text('Favorite'),
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.favorite),
+        title: Text('Favorite'),
+      ),
+    ];
 
-        final _listPage = <Widget>[
-          Text('Halaman Home'),
-          Text('Halaman Favorite'),
-          Text('Halaman Profile'),
-        ];
-
-        final _bottomNavBarItems = <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-          ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            title: Text('Favorite'),
-          ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title: Text('Profile'),
-          ),
-        ];
-
-        final _bottomNavBar = BottomNavigationBar (
-          items: _bottomNavBarItems,
-          currentIndex: _selectedTabIndex,
-          
-        )
-
-
-        
+    final _bottomNavBar = BottomNavigationBar(
+      items: _bottomNavBarItems,
+      currentIndex: _selectedTabIndex,
+      selectedItemColor: Colors.lightBlueAccent,
+      unselectedItemColor: Colors.grey,
+      onTap: _onNavBarTapped,
     );
+
+    return Scaffold(
+        appBar: AppBar(title: Text('Bottom Navigation Bar')),
+        body: Center(child: _listPage[_selectedTabIndex]),
+        bottomNavigationBar: _bottomNavBar);
   }
 }
