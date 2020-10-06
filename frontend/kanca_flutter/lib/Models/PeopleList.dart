39991ddcbbc3faf4a857c0ba20db5c67fde58bbb
bocahrokok <indieunistudy@gmail.com>
2020-10-06 22:29:38 +0700

    add people list

--- frontend/kanca_flutter/lib/Models/PeopleList.dart
+++ frontend/kanca_flutter/lib/Models/PeopleList.dart
@@ -1,24 +1,6 @@
 import 'package:tinder_clone/Widgets/MatchCard.dart';
 
 List<MatchCard> peoples = [
-<<<<<<< HEAD
-  MatchCard(
-      "Adam Livene", 'assets/images/person1.jpg', 21, 'Software Developer'),
-  // MatchCard("Derek Staham", 'assets/images/person2.jpg', 25,
-  //     'Engineer in Mechatronics'),
-  // MatchCard(
-  //     "Alexa Georigna", 'assets/images/person3.jpg', 23, 'Photographer 📷'),
-  // MatchCard(
-  //     "Maxii", 'assets/images/person4.jpg', 23, 'Camerographer 📷'),
-  // MatchCard(
-  //     "Risica Nibah", 'assets/images/person5.jpg', 26, 'Studying in W.A Eng.'),
-  // MatchCard(
-  //     "Christina", 'assets/images/person6.jpg', 34, 'Developer Advocate 👔'),
-  // MatchCard(
-  //     "Rissu Stelin", 'assets/images/person7.jpg', 23, 'Studying Aerospace 🛫'),
-  // MatchCard(
-  //     "Rebicca", 'assets/images/person8.jpg', 24, 'MIT Open Courseware 📚'),
-=======
   MatchCard("Ity", 'assets/images/nurrizky.jpg', 21, 'Software Developer'),
   MatchCard("Linus", 'assets/images/linus_2.png', 20, 'Engineer '),
   MatchCard("Jevon Edmund", 'assets/images/jevoned.png', 20, 'Student'),
@@ -31,5 +13,4 @@ List<MatchCard> peoples = [
       "Rissu Stelin", 'assets/images/person7.jpg', 23, 'Studying Aerospace 🛫'),
   MatchCard(
       "Rebicca", 'assets/images/person8.jpg', 24, 'MIT Open Courseware 📚'),
->>>>>>> 3d2c8646d800339d9ac7e79ce2156ba535decb08
 ];
